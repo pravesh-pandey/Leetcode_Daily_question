7e55bf9db8d07246d23729eff85e481ca3a24e9f
Pravesh Pandey <pravesh25pandey@gmail.com>
2020-10-19 13:16:08 +0530

    made custom hearder files for Leetcode

---
+++ custom_heder/hellow.h
@@ -0,0 +1,8 @@
+// class hellow
+// {
+//     public:
+    int find(int t)
+    {
+        return 2*t;
+    }
+// };
---
+++ hellow.cpp
@@ -0,0 +1,8 @@
+#include<bits/stdc++.h>
+#include"./custom_heder/hellow.h"
+using namespace std;
+int main()
+{
+
+    cout<<find(90);
+}
