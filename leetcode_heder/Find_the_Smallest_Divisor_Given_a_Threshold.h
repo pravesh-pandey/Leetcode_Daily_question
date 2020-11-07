aafc6aa3a6eb1046014f760db2d9b9a8200115e9
Pravesh Pandey <pravesh25pandey@gmail.com>
2020-11-07 12:51:38 +0530

    nov_6

--- leetcode_heder/Find_the_Smallest_Divisor_Given_a_Threshold.h
+++ leetcode_heder/Find_the_Smallest_Divisor_Given_a_Threshold.h
@@ -1,29 +1,41 @@
 #include<bits/stdc++.h>
 using namespace std;
-class Solution
-{
+class Solution {
 public:
-    int smallestDivisor(vector<int> &nums, int threshold)
-    {
-        int up_end = threshold, low_end = 0;
-        int diviser = up_end ;
-        int sum = 0;
-        while (diviser>low_end)
-        {
-            sum=0;
-            for (auto i : nums) 
+    int smallestDivisor(vector<int>& nums, int threshold) {
+        // lower bound binary search
+        
+        cin.tie(0);
+        cout.tie(0);
+        ios_base::sync_with_stdio(false);
+        
+        int n = nums.size();
+        
+        int low = 1;
+        int high = *max_element(nums.begin(), nums.end());
+        
+        while(low < high){
+            
+            int mid = low + (high-low)/2;
+            
+            int sum = 0;
+            
+            for(int i=0;i<n;i++)
             {
-                sum += ceil(double(double(i)/double(diviser)));
-            }
-            if (sum < threshold)
-                break;
-            else
-            {
-                diviser--;
-                
+                double q = (double)(nums[i]) / mid;
+                if(q - (int)q == 0.0) 
+                sum += (int)q;
+                else 
+                sum += (int)(q+1);
             }
             
+            if(sum > threshold){
+                low = mid+1;
+            }
+            else high = mid;   
+            
+            
         }
-        return sum;
+        return high;
     }
 };
