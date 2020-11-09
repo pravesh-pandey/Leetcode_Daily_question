dcd5b0478e9556d694f02b95daa34ce1b390bcbe
Pravesh Pandey <pravesh25pandey@gmail.com>
2020-11-09 11:58:20 +0530

    nov_9_2020

---
+++ leetcode_heder/Binary_Tree_Tilt.h
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+struct TreeNode {
+      int val;
+      TreeNode *left;
+      TreeNode *right;
+      TreeNode() : val(0), left(nullptr), right(nullptr) {}
+      TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+      TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+  };
+class Solution 
+{
+    int traverse(TreeNode *root,int * tilt)
+    {
+        if(!root)
+        return 0;
+        else
+        {
+           int left=traverse(root->left,tilt),right=traverse(root->right,tilt);
+           *tilt+=abs(left-right);
+            return (left+right+root->val);
+
+        }
+        
+    }
+    public:
+        int findTilt(TreeNode* root) 
+        {
+            int tilt = 0; 
+             traverse(root, &tilt); 
+            return tilt; 
+
+        }
+};
